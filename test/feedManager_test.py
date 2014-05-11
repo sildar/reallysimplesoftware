ddca12f45372ee1e51e713589ab11a453659b0a9
Sildar <sildar44@gmail.com>
2014-05-11 18:20:26 +0200

    Added test

---
+++ test/feedManager_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import sys, os
+sys.path.insert(0, os.path.dirname('..'))
+
+from reallysimplesoftware import feedManager
+
+class TestEntry(unittest.TestCase):
+
+    def setUp(self):
+        self.fakedata = {'title' : 'entrytitle',
+                         'published' : '24-06-04',
+                         'link' : 'http://www.fakerss.com',
+                         'content' : "This is a long entry for my fake content",
+        }
+
+        self.shortdata = {'title' : 'entrytitle',
+                         'published' : '24-06-04',
+                         'link' : 'http://www.fakerss.com',
+                         'summary' : "This is a summary of my entry",
+        } 
+    
+    def test_fillEntry(self):
+        """
+        Dumb test just for setting things up
+        """
+        entry = feedManager.Entry(self.fakedata)
+        self.assertEqual(self.fakedata['title'], entry.title)
+
+if __name__ == '__main__':
+    unittest.main()
